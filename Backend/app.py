9584766af39312a37a473245a1de7a796ab4437f
udaym <udaythedev@gmail.com>
2025-10-15 18:50:59 +0530

    minor update

--- Backend/app.py
+++ Backend/app.py
@@ -71,4 +71,5 @@ def serve(path):
 
 # --------- Run Server ---------
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
